6de5411d57e80fabdd10c3047d8314a972317c83
Mugisha Innocent <tafloninno@gmail.com>
2023-05-03 16:55:28 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 /* Database schema to keep the structure of entire database. */
-
 CREATE TABLE animals (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     name text COLLATE pg_catalog."default",
@@ -7,6 +6,7 @@ CREATE TABLE animals (
     escape_attempts integer,
     neutered boolean,
     weight_kg numeric,
-    species text COLLATE pg_catalog."default",
+    species varchar(255) COLLATE pg_catalog."default",
     CONSTRAINT animals_pkey PRIMARY KEY (id)
 );
+
